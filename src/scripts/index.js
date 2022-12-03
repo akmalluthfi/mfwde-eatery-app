f675fded478f7ffb9ae6f3b40a0d1e134d7a8a77
akmalluthfi <akmalluthfi19@gmail.com>
2022-12-03 14:56:26 +0700

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/modern-normalize.css";
+import "../styles/main.css";
+import "../styles/navbar.css";
+
+console.log("Hello Coders! :)");
