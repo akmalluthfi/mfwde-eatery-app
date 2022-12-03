039a1862da9027a68b9cf0a2c27126e1c7c44807
akmalluthfi <akmalluthfi19@gmail.com>
2022-12-03 19:16:14 +0700

    create navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,18 @@
 import "regenerator-runtime"; /* for async await transpile */
-import "../styles/modern-normalize.css";
+import "../styles/preflight.css";
 import "../styles/main.css";
 import "../styles/navbar.css";
 
-console.log("Hello Coders! :)");
+const navToggle = document.getElementById("navbar-toggle");
+navToggle.addEventListener("click", function () {
+	const navCollapse = document.querySelector(".navbar-collapse");
+	const navContainer = document.querySelector(".navbar-container");
+
+	navCollapse.classList.add("show");
+	navContainer.classList.add("d-contents");
+
+	navCollapse.addEventListener("click", function () {
+		navCollapse.classList.remove("show");
+		navContainer.classList.remove("d-contents");
+	});
+});
